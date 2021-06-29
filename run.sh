6687991e572f84f4e56c55c075e823f8089b25d9
Ludovic Muller <ludovicmuller1@gmail.com>
2021-06-29 13:06:12 +0200

    use relative path for the zazuko template

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 
 set -eux
 
-cp -r html/zazuko venv/share/jupyter/nbconvert/templates/
-jupyter nbconvert --to=html --template=zazuko notebooks/*.ipynb
+jupyter nbconvert --to=html --template=./html/zazuko notebooks/*.ipynb
 mv notebooks/*.html html/
 rsync -avr notebooks/img/ html/img
