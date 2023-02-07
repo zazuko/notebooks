457d8db491b6ff073c63fdbd4fbbb6e2cf1ee56d
magdalena@surowka.ch <magdalena@surowka.ch>
2023-02-07 11:05:39 +0100

    % diff on values

--- compare.sql
+++ compare.sql
@@ -1,19 +1,37 @@
 CREATE VIEW compare AS
-SELECT "Int".obs AS obs,
+SELECT
+"Int".obs AS obs,
+"Int"."gemeindeName" AS gemeinde,
+"Int".netzbetreiber AS netzbetreiber,
 "Int".total AS total_int,
 "Apps".total AS total_apps,
-"Int".total != "Apps".total AS is_total_different,
+CASE
+    WHEN "Apps".total = 0 THEN NULL
+    ELSE ("Apps".total-"Int".total)/"Apps".total
+END AS total_diff,
 "Int".energie AS energie_int,
 "Apps".energie AS energie_apps,
-"Apps".energie != "Int".energie AS is_energie_different,
+CASE
+    WHEN "Apps".energie = 0 THEN NULL
+    ELSE ("Apps".energie-"Int".energie)/"Apps".energie
+END AS energie_diff,
 "Int".abgaben AS abgaben_int,
 "Apps".abgaben AS abgaben_apps,
-"Int".abgaben != "Apps".abgaben AS is_abgaben_different,
+CASE
+    WHEN "Apps".abgaben = 0 THEN NULL
+    ELSE ("Apps".abgaben-"Int".abgaben)/"Apps".abgaben
+END AS abgaben_diff,
 "Int".netznutzung AS netznutzung_int,
 "Apps".netznutzung AS netznutzung_apps,
-"Int".netznutzung != "Apps".netznutzung AS is_netznutzung_different,
+CASE
+    WHEN "Apps".netznutzung = 0 THEN NULL
+    ELSE ("Apps".netznutzung-"Int".netznutzung)/"Apps".netznutzung
+END AS netznutzung_diff,
 "Int".foerderabgaben AS foerderabgaben_int,
 "Apps".foerderabgaben AS foerderabgaben_apps,
-"Apps".foerderabgaben != "Int".foerderabgaben AS is_foerderabgaben_different
+CASE
+    WHEN "Apps".foerderabgaben = 0 THEN NULL
+    ELSE ("Apps".foerderabgaben-"Int".foerderabgaben)/"Apps".foerderabgaben
+END AS foerderabgaben_diff
 FROM "Int"
 FULL OUTER JOIN "Apps" ON "Int".obs = "Apps".obs
